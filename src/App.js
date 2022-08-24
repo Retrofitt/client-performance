413cf16df3e4a8855606d1c851e4d5d396b9048c
Retrofitt <rafaelmendozajr94@gmail.com>
2022-08-24 13:12:28 -0700

    basic login component from antd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import "./styling/App.css";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm />
     </div>
   );
 }
